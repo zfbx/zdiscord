5d8f98da0ff9aa9e1be5096e4a5c9d87f45f53a5
Tony <1414927+zfbx@users.noreply.github.com>
2021-07-24 06:21:21 -0400

    qbcore add/remove permissions command

---
+++ zdiscord/commands/qb-removepermission.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'removepermission',
+    description: 'Remove a player\'s QB permissions',
+    args: `[${locale.helpTypeID}]`,
+    staffOnly: true,
+    run(discord, msg, args) {
+
+        let id = args.shift();
+        if (!id) return discord.createMessage(msg.channel.id, locale.noIdProvided);
+
+        id = Number(id);
+        if (isNaN(id)) return discord.createMessage(msg.channel.id, locale.invalidIdProvided);
+
+        if (!GetPlayerName(id)) return discord.createMessage(msg.channel.id, locale.invalidIdProvided);
+
+        let player = QBCore.Functions.GetPlayer(id);
+        if (!player) return discord.createMessage(msg.channel.id, "Player not found");
+
+        QBCore.Functions.RemovePermission(player.PlayerData.source)
+
+        console.log(`[${msg.nickname}] Removed ${id} permissions`);
+
+        msg.addReaction('✔');
+    },
+};
