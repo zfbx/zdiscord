73adf4896f4bfed9609a20f4601e56185700caa6
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 00:18:19 -0400

    Merge turkish translation by @alp1x

---
+++ zdiscord/locales/tr.js
@@ -0,0 +1,17 @@
+/* tr - Turkish Translation by alp1x ♥
+ Translation strings used by the bot
+ Strings wrapped in {{ }} will be auto-replaced with updated values
+ Globals: {{servername}}, {{invite}}, (all from config) and {{playercount}} (current connected players)
+*/
+const locale = {
+    checkingWhitelist: "Merhaba {{name}}, Whitelist kontrol ediliyor..",
+    discordNotOpen: "FiveM'i başlatmadan önce Discord açık olmalıdır. Sorun devam ederse ikisinide yeniden başlatın",
+    fatalError: "Whitelist durumunuz alınırken bir şeyler ters gitti, lütfen biraz sonra tekrar deneyin veya sorun devam ederse destek ile iletişime geçin.",
+    notInDiscordServer: "{{servername}} Discord adresinizde bulunmuyorsunuz, katılmak için: {{invite}}",
+    notWhitelisted: "{{servername}} için whitelist rolünüz bulunmamakta.",
+    kickedWithoutReason: "Yetkili tarafından sunucudan atıldın.",
+    announcement: "Duyuru",
+    directMessage: "Yetkili",
+};
+
+module.exports = locale;
