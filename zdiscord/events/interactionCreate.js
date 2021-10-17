431681d06f3078e0bc1877f4043814a3ee81a48f
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 06:52:08 -0400

    new event handlers (no more flat file)

---
+++ zdiscord/events/interactionCreate.js
@@ -0,0 +1,50 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    name: "interactionCreate",
+    run: async (client, interaction) => {
+        if (interaction.isCommand()) {
+            const command = client.commands.get(interaction.commandName);
+            if (!command) {
+                return interaction.reply({ content: "An error has occured ", ephemeral: true });
+            }
+            const args = [];
+            for (const option of interaction.options.data) {
+                if (option.type === "SUB_COMMAND") {
+                    if (option.name) args.push(option.name);
+                    option.options?.forEach((x) => {
+                        if (x.value) args.push(x.value);
+                    });
+                } else if (option.value) { args.push(option.value); }
+            }
+            interaction.member = interaction.guild.members.cache.get(interaction.user.id);
+            try {
+                command.run(client, interaction, args);
+            } catch (error) {
+                client.utils.log.error(error);
+                interaction.reply({ content: "There was an error while executing this command!", ephemeral: true }).catch(console.error);
+            }
+        }
+        if (interaction.isContextMenu()) {
+            const command = client.commands.get(interaction.commandName);
+            if (command) command.run(client, interaction);
+        }
+    },
+};
---
+++ zdiscord/events/ready.js
@@ -0,0 +1,53 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    name: "ready",
+    once: true,
+    run: async (client) => {
+        if (config.enableCommands) {
+            const guild = client.guilds.cache.get(client.config.guildid);
+            await guild.commands.set(client.arrayOfCommands).then((cmd) => {
+                const fullPermissions = cmd.reduce((accumulator, x) => {
+                    const command = client.arrayOfCommands.find((y) => y.name === x.name).role;
+                    if (!command) return accumulator;
+                    const permissions = client.config.perms[command];
+                    return [
+                        ...accumulator,
+                        { id: x.id, permissions },
+                    ];
+                }, []);
+                guild.commands.permissions.set({ fullPermissions });
+            });
+        }
+        if (config.enableStatus && config.statusMessages) statusUpdater(client);
+        client.utils.log.info(`Logged in as ${client.user.tag}`);
+    },
+};
+
+async function statusUpdater(client) {
+    setInterval(function() {
+        try {
+            const msg = client.utils.replaceGlobals(config.statusMessages[Math.floor(Math.random() * config.statusMessages.length)]);
+            client.user.setActivity({ name: msg, type: "PLAYING" });
+        } catch (e) {
+            // Just gonna void these errors..
+        }
+    }, 30000);
+}
