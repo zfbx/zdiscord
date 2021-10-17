5e0c74eb06412ca597558bcbff1c6a3f2d23d616
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 08:55:25 -0400

    add announcement command

---
+++ zdiscord/commands/announcement.js
@@ -0,0 +1,41 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    name: "announcement",
+    description: "Send in city announcement",
+    default_permission: false,
+    role: "mod",
+
+    options: [
+        {
+            name: "message",
+            description: "announcement to send",
+            required: true,
+            type: "STRING",
+        },
+    ],
+
+    run: async (client, interaction, args) => {
+        const [ message ] = args;
+        TriggerClientEvent("chatMessage", -1, client.locale.announcement, "error", message);
+        client.utils.log.info(`[${interaction.member.displayName}] Announcement: ${message}`);
+        interaction.reply({ content: "Announcement Sent", ephemeral: false });
+    },
+};
