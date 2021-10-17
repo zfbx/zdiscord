6a03c5ae7f5e390d526c77186ddaec53726e7e7c
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 00:22:18 -0400

    remove old commands

--- zdiscord/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    description: "Check bot status",
-    role: "none",
-    run(discord, msg, args) {
-        discord.createMessage(msg.channel.id, "Pong");
-    },
-};
