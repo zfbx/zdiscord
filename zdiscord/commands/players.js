796a4e6112e7ae1c7367b9fed57adab8332dfa35
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 08:56:57 -0400

    updated players command

--- zdiscord/commands/players.js
+++ zdiscord/commands/players.js
@@ -1,15 +1,56 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { MessageButton } = require("discord.js");
+
 module.exports = {
     name: "players",
-    alias: "p",
-    description: "Get list of players in the city",
+    description: "Get list of current players in city",
+    default_permission: false,
     role: "mod",
-    run(discord, msg, args) {
 
-        if (GetNumPlayerIndices() === 0) return discord.createMessage(msg.channel.id, "Nobody is online.");
-        const playersembed = { embed: { color: config.embedColor, fields: [{ name: "Players ({{playercount}})".replaceGlobals(), value: "", inline: false }] } }
-        getPlayers().forEach(async function (player, index, array) {
-            playersembed.embed.fields[0].value = playersembed.embed.fields[0].value + `\`[${player}]\` ${GetPlayerName(player)}\n`;
+    run: async (client, interaction) => {
+        if (GetNumPlayerIndices() === 0) return interaction.reply({ content: "Nobody is currently online to pull", ephemeral: false });
+        const parts = [];
+        let index = 0;
+        getPlayers().sort().forEach((id) => {
+            const i = Math.floor(index / 10);
+            if (!parts[i]) parts[i] = "";
+            parts[i] += `\`[${id}]\` **${GetPlayerName(id)}**`;
+            if (client.QBCore) {
+                try {
+                    const player = client.QBCore.Functions.GetPlayer(parseInt(id));
+                    console.log(player);
+                    parts[i] += ` | (${player.PlayerData.citizenid}) **${player.PlayerData.charinfo.firstname} ${player.PlayerData.charinfo.lastname}**\n`;
+                } catch { parts[i] += " (Not yet loaded)\n"; }
+            } else { parts[i] += "\n"; }
+            index++;
         });
-        return discord.createMessage(msg.channel.id, playersembed);
+        const pages = [];
+        parts.forEach((part) => {
+            const embed = new client.Embed()
+                .setTitle(`Players (${GetNumPlayerIndices()})`)
+                .setDescription(`${part}`);
+            pages.push(embed);
+        });
+        const backBtn = new MessageButton().setCustomId("previousbtn").setEmoji("🔺").setStyle("SECONDARY");
+        const forwardBtn = new MessageButton().setCustomId("nextbtn").setEmoji("🔻").setStyle("SECONDARY");
+        client.utils.paginationEmbed(interaction, pages, [backBtn, forwardBtn]);
     },
 };
