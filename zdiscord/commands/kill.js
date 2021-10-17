a4cddb1db1f006ceb550df890d98976af0422448
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 14:04:24 -0400

    new kill command

---
+++ zdiscord/commands/kill.js
@@ -0,0 +1,42 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+module.exports = {
+    name: "kill",
+    description: "kill a player in city",
+    default_permission: false,
+    role: "admin",
+
+    options: [
+        {
+            name: "id",
+            description: "Player's current id",
+            required: true,
+            type: "INTEGER",
+        },
+    ],
+
+    run: async (client, interaction, args) => {
+        const [ id ] = args;
+        if (!GetPlayerName(id)) return interaction.reply({ content: "This ID seems invalid.", ephemeral: true });
+        TriggerClientEvent("zdiscord:kill", id);
+        client.utils.log.info(`[${interaction.member.displayName}] Killed ${GetPlayerName(id)} (${id})`);
+        return interaction.reply({ content: `${GetPlayerName(id)} (${id}) has been murdered.`, ephemeral: false });
+    },
+};
