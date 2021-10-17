b928f466d781efb904203dacf4c395186dd437a6
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 14:05:30 -0400

    kick and kickall command rewrites

--- zdiscord/commands/kick.js
+++ zdiscord/commands/kick.js
@@ -1,26 +1,49 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 module.exports = {
     name: "kick",
-    alias: "k",
-    description: "Kicks user in city",
-    args: `[id] (reason)`,
+    description: "Kick a player from the city",
+    default_permission: false,
     role: "mod",
-    run(discord, msg, args) {
 
-        let id = args.shift();
-        if (!id) return discord.createMessage(msg.channel.id, "You must provide an ID of a player.");
+    options: [
+        {
+            name: "id",
+            description: "Player's current id",
+            required: true,
+            type: "INTEGER",
+        },
+        {
+            name: "message",
+            description: "Kick message to show the user",
+            required: false,
+            type: "STRING",
+        },
+    ],
 
-        id = Number(id);
-        if (isNaN(id)) return discord.createMessage(msg.channel.id, "This ID seems invalid.");
-
-        let content = args.join(" ") || "";
-        if (!content) content = locale.kickedWithoutReason.replaceGlobals();
-
-        if (!GetPlayerName(id)) return discord.createMessage(msg.channel.id, "This ID seems invalid.");
-
-        DropPlayer(id, content);
-
-        console.log(`[${msg.nickname}] Kicked ${id}. Reason: ${content}`);
-
-        msg.addReaction('✅');
+    run: async (client, interaction, args) => {
+        const [ id, message ] = args;
+        if (!GetPlayerName(id)) return interaction.reply({ content: "This ID seems invalid.", ephemeral: true });
+        const reason = client.utils.replaceGlobals(message || client.locale.kickedWithoutReason);
+        DropPlayer(id, reason);
+        client.utils.log.info(`[${interaction.member.displayName}] Kicked ${GetPlayerName(id)} (${id}). Reason: ${reason}`);
+        return interaction.reply({ content: `${GetPlayerName(id)} (${id}) has been kicked.`, ephemeral: false });
     },
 };
--- zdiscord/commands/kickall.js
+++ zdiscord/commands/kickall.js
@@ -1,22 +1,45 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 module.exports = {
     name: "kickall",
-    description: "Kicks everyone in city",
-    args: `[reason]`,
+    description: "Kick every player in the city",
+    default_permission: false,
     role: "admin",
-    run(discord, msg, args) {
 
+    options: [
+        {
+            name: "message",
+            description: "Kick message to show the user",
+            required: true,
+            type: "STRING",
+        },
+    ],
+
+    run: async (client, interaction, args) => {
+        const [ message ] = args;
         const numberOnline = GetNumPlayerIndices();
-        if (numberOnline === 0) return discord.createMessage(msg.channel.id, "Nobody is online.");
-
-        const content = args.join(" ") || "";
-        if (!content) return discord.createMessage(msg.channel.id, "Please provide a message");
-
-        getPlayers().forEach(async function (player, index, array) {
-            DropPlayer(player, content);
+        if (numberOnline === 0) return interaction.reply({ content: "Nobody was online to kick.", ephemeral: false });
+        getPlayers().forEach(async (player) => {
+            DropPlayer(player, message);
         });
-
-        discord.createMessage(msg.channel.id, `All ${numberOnline} player(s) have kicked.`);
-
-        console.log(`[${msg.nickname}] Kicked EVERYONE. Reason: ${content}`);
+        client.utils.log.info(`[${interaction.member.displayName}] Kicked all ${numberOnline} player(s). Reason: ${reason}`);
+        return interaction.reply({ content: `All ${numberOnline} player(s) have been kicked.`, ephemeral: false });
     },
 };
