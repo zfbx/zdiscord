0748d67790a5a8fea7fccf1d22bfe82d0f2d029a
Tony <1414927+zfbx@users.noreply.github.com>
2021-10-17 14:03:04 -0400

    revive rewrite - needs testing

--- zdiscord/commands/qb-revive.js
+++ zdiscord/commands/qb-revive.js
@@ -1,26 +1,42 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * zdiscord is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * zdiscord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with zdiscord. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 module.exports = {
-    name: 'revive',
-    alias: 'r',
-    description: 'Revive player in city',
-    args: `[id]`,
+    name: "revive",
+    description: "Raise a downed player to full health and stats",
+    default_permission: false,
     role: "admin",
-    run(discord, msg, args) {
 
-        let id = args.shift();
-        if (!id) return discord.createMessage(msg.channel.id, "You must provide an ID of a player.");
+    options: [
+        {
+            name: "id",
+            description: "Player's current id",
+            required: true,
+            type: "INTEGER",
+        },
+    ],
 
-        id = Number(id);
-        if (isNaN(id)) return discord.createMessage(msg.channel.id, "This ID seems invalid.");
-
-        if (!GetPlayerName(id)) return discord.createMessage(msg.channel.id, "This ID seems invalid.");
-
-        let player = QBCore.Functions.GetPlayer(id);
-
-        TriggerClientEvent('hospital:client:Revive', player.PlayerData.source);
-
-        console.log(`[${msg.nickname}] Revived ${id}`);
-
-        msg.addReaction('✅');
-
+    run: async (client, interaction, args) => {
+        const [ id ] = args;
+        if (!GetPlayerName(id)) return interaction.reply({ content: "This ID seems invalid.", ephemeral: true });
+        TriggerClientEvent("hospital:client:Revive", id);
+        client.utils.log.info(`[${interaction.member.displayName}] revived ${GetPlayerName(id)} (${id})`);
+        return interaction.reply({ content: `${GetPlayerName(id)} (${id}) has been fully healed.`, ephemeral: false });
     },
 };
