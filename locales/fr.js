fe9b7355c3e2d6aadac5d822ef927ac42f2c6cea
Tony <1414927+zfbx@users.noreply.github.com>
2021-12-08 10:32:48 -0500

    Update fr.js

--- locales/fr.js
+++ locales/fr.js
@@ -1,4 +1,4 @@
-/* en - English Translation by zfbx ♥
+/* fr - French Translation by tiweb442
  Translation strings used by the bot
  Strings wrapped in { } will be auto-replaced with updated values
  Globals: {servername}, {invite}, (all from config) and {playercount} (current connected players)
