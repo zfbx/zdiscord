605a56c217a2e2172c860f2c880729a77a5d74bd
Tony <1414927+zfbx@users.noreply.github.com>
2022-01-16 01:38:35 -0500

    example empty discord events

---
+++ server/events/inviteCreate.js
@@ -0,0 +1,17 @@
+/**
+ * This file is part of zdiscord.
+ * Copyright (C) 2021 Tony/zfbx
+ * source: <https://github.com/zfbx/zdiscord>
+ *
+ * This work is licensed under the Creative Commons
+ * Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
+ * or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
+module.exports = {
+    name: "inviteCreate",
+    run: async (client, invite) => {
+        // https://discord.js.org/#/docs/discord.js/stable/class/Client?scrollTo=e-inviteCreate
+    },
+};
